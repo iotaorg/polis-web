6a610ca92b73885dc946c41341f90d2b167969b6
RenatoCRON <renato.cron@gmail.com>
2016-07-28 16:14:15 -0300

    polis php docker

--- run-container.sh
+++ run-container.sh
@@ -3,4 +3,6 @@
 export GIT_DIR=$(git rev-parse --show-toplevel)
 export MYIP=$(ip route get 8.8.8.8 | head -1 | cut -d' ' -f8)
 
-docker run -d -p 5050:80 --add-host=api_server:$MYIP --name website-php-7 -v "$GIT_DIR":/var/www/html cron-php7-rewrite
+docker kill polis-php
+docker rm polis-php
+docker run -d -p 5050:80 --add-host=api_server:$MYIP --name polis-php -v "$GIT_DIR":/var/www/html cron-php7-rewrite
