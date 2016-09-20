1cd06a0cfe62eaa1372c38c206129a6be2f68c5c
RenatoCRON <renato.cron@gmail.com>
2016-09-20 17:23:29 -0300

    destaque nos Litorais e ordem (apenas tabela de valores)

--- templates/segment/indicador_tabela_rot_regiao.php
+++ templates/segment/indicador_tabela_rot_regiao.php
@@ -4,7 +4,7 @@
 <tr>
     <th>Ano</th>
     <? foreach ( $dados->headers as $h ): ?>
-        <th><?=$h->v?></th>
+        <th <?=( $h->b ==1 ) ? 'class="bold"':''?>><?=$h->v?></th>
     <?endforeach?>
 </tr>
 </thead>
@@ -16,7 +16,7 @@
         <th><?=$l->v?></th>
 
         <? foreach ( $dados->headers as $h ): $v = !isset($dados->data->{  $l->k }->{ $h->k })  ? '' : $dados->data->{  $l->k }->{ $h->k }; ?>
-        <td><?if (isset($v)): ?><?=$dados->indicator->prepend_on_result ?> <?= (  !is_numeric($v) ? ($v ? $v : '-') : str_replace(',00', '', number_format((float)$v, 2, ',', '.')) ) ?> <?=@$dados->indicator->append_on_result ?><?endif?></td>
+        <td <?=( $h->b ==1 ) ? 'class="bold"':''?>><?if (isset($v)): ?><?=$dados->indicator->prepend_on_result ?> <?= (  !is_numeric($v) ? ($v ? $v : '-') : str_replace(',00', '', number_format((float)$v, 2, ',', '.')) ) ?> <?=@$dados->indicator->append_on_result ?><?endif?></td>
         <?endforeach?>
 
     </tr>
